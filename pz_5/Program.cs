1dbeb5c25b4001b2f3a2c847516d31779979c5e4
сергей козловцев <blackatom21@mail.ru>
2022-09-22 09:46:56 +0500

    pz5

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -9,12 +9,33 @@ namespace pz_5
     internal class Program
     {
         static void Main(string[] args)
-
         {
-            for (int i = 0; i <= 80; i += 4)
-            {
-                Console.WriteLine(i);
-            }
+
+            for (int i = 1; i <; i++)
+                for (int j=40; j<)
+
+
+
+            //for (int i = 0; i <=100 ; i++)
+            //    if (i % 5 == 0)
+            //        Console.WriteLine(i);                         //#4
+
+
+
+            //            char a = 'c';
+            //            for (char i = a; i <= 'j'; i++)
+            //                Console.Write(i);                     //#2
+
+
+
+
+            //            for (int i = 0; i <= 80; i += 4)
+            //                Console.WriteLine(i);                 //#1
+
+
+
         }
     }
 }
+
+
