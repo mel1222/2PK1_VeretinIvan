f067dbbb4b451ea45eea790a39ceb48d5f7e0abd
mel .lnst <veretin_04@mail.ru>
2022-09-22 20:59:03 +0500

    pz_5

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -12,51 +12,50 @@ namespace pz_5
         static void Main(string[] args)
         {
             {
-                for(int j = 40, i = 1; j - i >= 15; j--, i++)
+                for (int i = 1; i <= 80; i += 4)
                 {
-                    Console.WriteLine(j - i);
+                    Console.WriteLine(i);
                 }
-                Console.ReadKey();
+                Console.WriteLine("Первое задание выполнено");
+
+
+                for (char x = 'c'; x <= 'j'; x++)
+                {
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine("Второе задание выполнено");
+
+                int n = 6;
+                int m = 5;
+                for (int i = 0; i < m; i++)
+                {
+                    for (int x = 0; x < n; x++)
+                    {
+                        Console.Write('#');
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Третье задание выполнено");
+
+
+                for (int i = 0; i <= 100; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i + " ");
+                    }
+                }
+                Console.WriteLine("Четвертое задание выполнено");
+
+
+                for (int i = 1, j = 40; i - j <= 15; i++, j--)
+                {
+                    Console.WriteLine(i - j);
+                }
+                Console.WriteLine("Пятое задание выполнено");
             }
-
-
-
-
-
-
-            //int n = 6;
-            //int m = 5;
-            //for (int i = 0; i < m; i++)
-            //{
-            //    for (int x = 0; x < n; x++)
-            //    {
-                 
-            //        Console.Write('#');
-            //    }
-            //    Console.WriteLine();                              //#3
-            }
-
-
-            //for (int i = 0; i <=100 ; i++)
-            //    if (i % 5 == 0)
-            //        Console.WriteLine(i);                         //#4
-
-
-
-            //            char a = 'c';
-            //            for (char i = a; i <= 'j'; i++)
-            //                Console.Write(i);                     //#2
-
-
-
-
-            //            for (int i = 0; i <= 80; i += 4)
-            //                Console.WriteLine(i);                 //#1
-
-
 
         }
     }
-
-
+}
 
