ca61f52e851fb0851a93af5af20073e12f31a30b
mel .lnst <veretin_04@mail.ru>
2022-09-26 19:09:52 +0500

    pz_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите натуральное число не меньше двух: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            while (a % i != 0)
+            {
+                i++;
+            }
+            Console.WriteLine(i);
+            Console.ReadKey();
+
+        }
+    }
+}
